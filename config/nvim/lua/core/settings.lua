3030d6faa17a67c9104ade1948ba20ba7dc5ed2a
Rintaro Itokawa <me@re-taro.dev>
2023-06-10 12:46:31 +0900

    update nvim settings

--- config/nvim/lua/core/settings.lua
+++ config/nvim/lua/core/settings.lua
@@ -3,15 +3,27 @@ local home = require("core.global").home
 
 -- Set it to false if you want to use https to update plugins and treesitter parsers.
 ---@type boolean
-settings["use_ssh"] = false
+settings["use_ssh"] = true
 
 -- Set it to false if there are no need to format on save.
 ---@type boolean
 settings["format_on_save"] = true
 
--- Set it to false if diagnostics virtual text is annoying for you
+-- Set it to false if the notification after formatting is annoying.
 ---@type boolean
-settings["diagnostics_virtual_text"] = true
+settings["format_notify"] = true
+
+-- Set it to false if diagnostics virtual text is annoying.
+-- If disabled, you may browse lsp diagnostics using trouble.nvim (press `gt` to toggle it).
+---@type boolean
+settings["diagnostics_virtual_text"] = false
+
+-- Set it to one of the values below if you want to change the visible severity level of lsp diagnostics.
+-- Priority: `Error` > `Warning` > `Information` > `Hint`.
+--  > e.g. if you set this option to `Warning`, only lsp warnings and errors will be shown.
+-- NOTE: This entry only works when `diagnostics_virtual_text` is true.
+---@type "Error"|"Warning"|"Information"|"Hint"
+settings["diagnostics_level"] = "Hint"
 
 -- Set the format disabled directories here, files under these dirs won't be formatted on save.
 ---@type string[]
@@ -51,22 +63,21 @@ settings["background"] = "dark"
 ---@type string
 settings["external_browser"] = "chrome-cli open"
 
--- Filetypes in this list will skip lsp formatting if rhs is true
+-- Filetypes in this list will skip lsp formatting if rhs is true.
 ---@type table<string, boolean>
 settings["formatter_block_list"] = {
     lua = false, -- example
 }
 
--- Servers in this list will skip setting formatting capabilities if rhs is true
+-- Servers in this list will skip setting formatting capabilities if rhs is true.
 ---@type table<string, boolean>
 settings["server_formatting_block_list"] = {
     lua_ls = true,
     tsserver = true,
     clangd = true,
-    pylsp = true,
 }
 
--- Set the language servers that will be installed during bootstrap here
+-- Set the language servers that will be installed during bootstrap here.
 -- check the below link for all the supported LSPs:
 -- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/server_configurations
 ---@type string[]
@@ -80,13 +91,12 @@ settings["lsp_deps"] = {
     -- "gopls",
 }
 
--- Set the general-purpose servers that will be installed during bootstrap here
--- check the below link for all supported sources
+-- Set the general-purpose servers that will be installed during bootstrap here.
+-- Check the below link for all supported sources.
 -- in `code_actions`, `completion`, `diagnostics`, `formatting`, `hover` folders:
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins
 ---@type string[]
 settings["null_ls_deps"] = {
-    "black",
     "clang_format",
     "prettier",
     "rustfmt",
