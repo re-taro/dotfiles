289639684370c1b86f46d8f3d87815401d3f94c4
Rintaro Itokawa <me@re-taro.dev>
2023-11-03 14:09:17 +0900

    update

--- config/nvim/lua/core/settings.lua
+++ config/nvim/lua/core/settings.lua
@@ -4,6 +4,10 @@ local settings = {}
 ---@type boolean
 settings["use_ssh"] = true
 
+-- Set it to false if you don't use copilot
+---@type boolean
+settings["use_copilot"] = true
+
 -- Set it to false if there are no need to format on save.
 ---@type boolean
 settings["format_on_save"] = true
@@ -12,9 +16,22 @@ settings["format_on_save"] = true
 ---@type boolean
 settings["format_notify"] = true
 
--- Set it to false if you don't use copilot
+-- Set it to true if you prefer formatting ONLY the *changed lines* as defined by your version control system.
+-- NOTE: This entry will only be respected if:
+--  > The buffer to be formatted is under version control (Git or Mercurial);
+--  > Any of the server attached to that buffer supports |DocumentRangeFormattingProvider| server capability.
+-- Otherwise Neovim would fall back to format the whole buffer, and a warning will be issued.
 ---@type boolean
-settings["use_copilot"] = true
+settings["format_modifications_only"] = false
+
+-- Set the format disabled directories here, files under these dirs won't be formatted on save.
+--- NOTE: Directories may contain regular expressions (grammar: vim). |regexp|
+--- NOTE: Directories are automatically normalized. |vim.fs.normalize()|
+---@type string[]
+settings["format_disabled_dirs"] = {
+    -- Example
+    "~/format_disabled_dir",
+}
 
 -- Set it to false if diagnostics virtual text is annoying.
 -- If disabled, you may browse lsp diagnostics using trouble.nvim (press `gt` to toggle it).
@@ -27,14 +44,6 @@ settings["diagnostics_virtual_text"] = true
 -- NOTE: This entry only works when `diagnostics_virtual_text` is true.
 ---@type "Error"|"Warning"|"Information"|"Hint"
 settings["diagnostics_level"] = "Hint"
-
--- Set the format disabled directories here, files under these dirs won't be formatted on save.
---- NOTE: Directories may contain regular expressions (grammar: vim). |regexp|
---- NOTE: Directories are automatically normalized. |vim.fs.normalize()|
----@type string[]
-settings["format_disabled_dirs"] = {
-    "~/format_disabled_dir",
-}
 
 -- Set the plugins to disable here.
 -- Example: "Some-User/A-Repo"
@@ -53,7 +62,7 @@ settings["load_big_files_faster"] = true
 settings["palette_overwrite"] = {}
 
 -- Set the colorscheme to use here.
--- Available values are: `catppuccin`, `catppuccin-latte`, `catppucin-mocha`, `catppuccin-frappe`, `catppuccin-macchiato`, `edge`, `nord`.
+-- Available values are: `catppuccin`, `catppuccin-latte`, `catppucin-mocha`, `catppuccin-frappe`, `catppuccin-macchiato`.
 ---@type string
 settings["colorscheme"] = "catppuccin"
 
@@ -107,6 +116,8 @@ settings["lsp_deps"] = {
 ---@type string[]
 settings["null_ls_deps"] = {
     "clang_format",
+    "gofumpt",
+    "goimports",
     "prettier",
     "shfmt",
     "stylua",
@@ -121,6 +132,59 @@ settings["dap_deps"] = {
     "codelldb", -- C-Family
     "delve", -- Go
     "python", -- Python (debugpy)
+}
+
+-- Set the Treesitter parsers that will be installed during bootstrap here.
+-- Check the below link for all supported languages:
+-- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
+---@type string[]
+settings["treesitter_deps"] = {
+    "bash",
+    "c",
+    "cpp",
+    "css",
+    "go",
+    "gomod",
+    "html",
+    "javascript",
+    "json",
+    "latex",
+    "lua",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "rust",
+    "typescript",
+    "vimdoc",
+    "vue",
+    "yaml",
+}
+
+-- Set the options for neovim's gui clients like `neovide` and `neovim-qt` here.
+-- NOTE: Currently, only the following options related to the GUI are supported. Other entries will be IGNORED.
+---@type { font_name: string, font_size: number }
+settings["gui_config"] = {
+    font_name = "JetBrainsMono Nerd Font",
+    font_size = 12,
+}
+
+-- Set the options specific to `neovide` here.
+-- NOTE: You should remove the `neovide_` prefix (with trailing underscore) from all your entries below.
+-- Check the below link for all supported entries:
+-- https://neovide.dev/configuration.html
+---@type table<string, boolean|number|string>
+settings["neovide_config"] = {
+    no_idle = true,
+    refresh_rate = 120,
+    cursor_vfx_mode = "railgun",
+    cursor_vfx_opacity = 200.0,
+    cursor_antialiasing = true,
+    cursor_trail_length = 0.05,
+    cursor_animation_length = 0.03,
+    cursor_vfx_particle_speed = 20.0,
+    cursor_vfx_particle_density = 5.0,
+    cursor_vfx_particle_lifetime = 1.2,
 }
 
 return require("modules.utils").extend_config(settings, "user.settings")
--- config/nvim/lua/modules/configs/ui/catppuccin.lua
+++ config/nvim/lua/modules/configs/ui/catppuccin.lua
@@ -58,7 +58,7 @@ return function()
             dropbar = { enabled = true, color_mode = true },
             fern = false,
             fidget = true,
-            flash = false,
+            flash = true,
             gitgutter = false,
             gitsigns = true,
             harpoon = false,
@@ -118,8 +118,8 @@ return function()
                     MasonNormal = { link = "NormalFloat" },
 
                     -- For indent-blankline
-                    IndentBlanklineChar = { fg = cp.surface0 },
-                    IndentBlanklineContextChar = { fg = cp.surface2, style = { "bold" } },
+                    IblIndent = { fg = cp.surface0 },
+                    IblScope = { fg = cp.surface2, style = { "bold" } },
 
                     -- For nvim-cmp and wilder.nvim
                     Pmenu = { fg = cp.overlay2, bg = transparent_background and cp.none or cp.base },
